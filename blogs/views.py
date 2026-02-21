ded6e420c4f19e9d6d0cd829def316b60703d400
Gian Marco <gianm1905@gmail.com>
2026-02-21 12:19:53 -0500

    First commit. Implemented: Navbar, Footer, 404, Categories, Blogs

---
+++ blogs/views.py
@@ -0,0 +1,31 @@
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+
+from blogs.models import Blog, Category
+
+
+# Views Here
+def posts_by_category(request, category_id):
+    # Fetch the posts that belongs to the category with the id category_id
+    posts = Blog.objects.filter(status='Published', category=category_id)
+    # Use try/except when you want to do some custom action if the category does not exist
+    # try:
+    #     category = Category.objects.get(pk=category_id)
+    # except:
+    #     return redirect('home') # redirect the user to homepage
+    # Use get_object_or_404 when you want to show 404 page if the category does  ot exist
+
+    category = get_object_or_404(Category, pk=category_id)
+
+    context = {
+        'posts': posts,
+        'category': category,
+    }
+    return render(request, 'posts_by_category.html', context)
+
+def blogs(request, slug):
+    single_blog = get_object_or_404(Blog, slug=slug, status='Published')
+    context = {
+        'single_blog': single_blog,
+    }
+    return render(request, 'blogs.html', context)
